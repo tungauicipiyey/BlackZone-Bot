2dc52adbd049a3f151a62def710cf8b556ee7bda
Zora24 <liualvinas1212@gmail.com>
2021-03-22 04:09:25 +0000

    pylint: auto fixes
    Signed-off-by: Zora24 <liualvinas1212@gmail.com>

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -106,8 +106,7 @@ async def pingme(pong):
     await pong.edit("**PING!**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**ㅤㅤㅤ  ≛ 𝙲𝙾𝚁𝙾𝙽𝙰 𝚅𝙸𝚁𝚄𝚂 ≛** \n ㅤ**▰▱▰▱▰▱▰▱▰▱▰▱▰** \n╭╼═════════════════╾╮\n⌲Ping Virus  :  %s𝖒𝖘\n⌲Virus Aktif  :  {uptime} 𝐎𝐍⊨〛\n⌲My Boss   : ㅤ`{ALIVE_NAME}`\n╰╼═════════════════╾╯ " % (duration))              
-
+    await pong.edit(f"**ㅤㅤㅤ  ≛ 𝙲𝙾𝚁𝙾𝙽𝙰 𝚅𝙸𝚁𝚄𝚂 ≛** \n ㅤ**▰▱▰▱▰▱▰▱▰▱▰▱▰** \n╭╼═════════════════╾╮\n⌲Ping Virus  :  %s𝖒𝖘\n⌲Virus Aktif  :  {uptime} 𝐎𝐍⊨〛\n⌲My Boss   : ㅤ`{ALIVE_NAME}`\n╰╼═════════════════╾╯ " % (duration))
 
 
 @register(outgoing=True, pattern="^.sinyal$")
