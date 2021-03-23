7190f695f936b46473cef73c1f0c8b54a01b6a4f
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-03-23 12:17:08 +0000

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -107,7 +107,7 @@ async def pingme(pong):
     await pong.edit("**PING..!**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**ㅤㅤㅤ  ≛ 𝙲𝙾𝚁𝙾𝙽𝙰 𝚅𝙸𝚁𝚄𝚂 ≛** \n ㅤ**▰▱▰▱▰▱▰▱▰▱▰▱▰** \n**╭╼═✯═══════✯══════✯═╾╮**\n⌲Ping Virus  : %s𝖒𝖘\n⌲Virus Aktif  : {uptime}\n⌲My Boss   : `{ALIVE_NAME}`\n**╰╼═✯═══════✯══════✯═╾╯** " % (duration))
+    await pong.edit(f"**ㅤㅤㅤ  ≛ 𝙲𝙾𝚁𝙾𝙽𝙰 𝚅𝙸𝚁𝚄𝚂 ≛** \n ㅤ**▰▱▰▱▰▱▰▱▰▱▰▱▰** \n**╔══════════✹══════════╗**\n➢Ping Virus  : %s𝖒𝖘\n➢Virus Aktif  : {uptime}\n➢My Boss   : `{ALIVE_NAME}`\n**╚══════════✹══════════╝** " % (duration))
 
 
 @register(outgoing=True, pattern="^.sinyal$")
