db0094cba6d47ed3ebca68d57aedd4e5aa3efd50
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-03-28 19:13:17 +0700

    Update lorddeteksi.py

--- userbot/modules/lorddeteksi.py
+++ userbot/modules/lorddeteksi.py
@@ -15,7 +15,7 @@ async def _(event):
     input_str = "".join(event.text.split(maxsplit=1)[1:])
     reply_message = await event.get_reply_message()
     if not event.reply_to_msg_id:
-        await event.edit("```Lord, Mohon Balas Ke Pesan Pengguna atau ketik .deteksi (ID/Username) Yang mau Anda deteksi```")
+        await event.edit("```Mohon Balas Ke Pesan Pengguna atau ketik .deteksi (ID/Username) Yang mau Anda deteksi```")
         return
     if input_str:
         try:
@@ -24,7 +24,7 @@ async def _(event):
             try:
                 u = await event.client.get_entity(input_str)
             except ValueError:
-                await edit.event("`Lord, Mohon Berikan ID/Username untuk menemukan Riwayat`"
+                await edit.event("`Mohon Berikan ID/Username untuk menemukan Riwayat`"
                                  )
             uid = u.id
     else:
@@ -36,7 +36,7 @@ async def _(event):
             await conv.send_message(f"{uid}")
         except YouBlockedUserError:
             await steal.reply(
-                "```Lord Mohon Unblock @tgscanrobot Dan Coba Lagi```"
+                "```Mohon Unblock @tgscanrobot Dan Coba Lagi```"
             )
         response = await conv.get_response()
         await event.client.send_read_acknowledge(conv.chat_id)
