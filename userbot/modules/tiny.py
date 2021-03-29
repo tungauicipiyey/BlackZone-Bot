610c4706933e5271f456a4879d6a80a76c2c5073
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-03-29 13:27:39 +0700

    Update tiny.py

--- userbot/modules/tiny.py
+++ userbot/modules/tiny.py
@@ -17,7 +17,7 @@ async def ultiny(event):
         return
     xx = await event.edit("`TinyTiny OinkOink🐷...`")
     ik = await bot.download_media(reply)
-    im1 = Image.open("VirusUserBot/virususerbot.png")
+    im1 = Image.open("Virus-UserBot/virususerbot.png")
     if ik.endswith(".tgs"):
         await event.client.download_media(reply, "ult.tgs")
         os.system("lottie_convert.py ult.tgs json.json")
