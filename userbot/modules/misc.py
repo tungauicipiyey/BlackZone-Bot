cdd141503cbc78e6158edf9747e9bd6be464ed13
Zora24 <liualvinas1212@gmail.com>
2021-04-21 20:49:28 +0000

    pylint: auto fixes
    Signed-off-by: Zora24 <liualvinas1212@gmail.com>

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -124,14 +124,12 @@ async def repo_is_here(wannasee):
     )
 
 
-
 @register(outgoing=True, pattern="^.string$")
 async def repo_is_here(wannasee):
     """ For .string command, just returns the string URL. """
     await wannasee.edit(
         " **Get String:** [Press For GetString](https://repl.it/@ManusiaRakitan/stringsession#README.md)"
     )
-
 
 
 @register(outgoing=True, pattern="^.instagram$")
