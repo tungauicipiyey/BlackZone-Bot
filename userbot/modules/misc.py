bfb9101120af6d9c0e0ba6b43a3c9af81de9d65c
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-04-22 03:56:35 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -136,7 +136,7 @@ async def repo_is_here(wannasee):
 async def repo_is_here(wannasee):
     """ For .instagram command, just returns the instagram URL. """
     await wannasee.edit(
-        " **INSTAGRAM  :**  [ARI MULYA](https://www.instagram.com/arimulyaa_)"
+        " **INSTAGRAM  :**  [KLIK FOR MY INSTAGRAM](https://www.instagram.com/arimulyaa_)"
     )
 
 
