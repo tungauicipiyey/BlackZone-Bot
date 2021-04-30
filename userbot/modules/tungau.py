6bf76d8d10d06c5c1b2390ce8ca79b647f42300c
Zora24 <liualvinas1212@gmail.com>
2021-04-30 17:37:52 +0000

    pylint: auto fixes
    Signed-off-by: Zora24 <liualvinas1212@gmail.com>

--- userbot/modules/tungau.py
+++ userbot/modules/tungau.py
@@ -6,22 +6,9 @@
 # You can find misc modules, which dont fit in anything xD
 """ Userbot module for other small commands. """
 
-from random import randint
-from time import sleep
-from os import execl
-import asyncio
-import sys
-import os
-import io
-import sys
-from userbot import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
+from userbot import ALIVE_NAME, CMD_HELP
 from userbot.events import register
-from userbot.utils import time_formatter
 import urllib
-import requests
-from bs4 import BeautifulSoup
-import re
-from PIL import Image
 
 
 # ================= CONSTANT =================
@@ -31,7 +18,6 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 opener = urllib.request.build_opener()
 useragent = 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.70 Mobile Safari/537.36'
 opener.addheaders = [('User-agent', useragent)]
-
 
 
 @register(outgoing=True, pattern="^.ainna$")
