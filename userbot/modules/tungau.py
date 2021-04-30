bbc767e77e58fa6e67be9b9b7036d8b294f4809c
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-05-01 00:24:33 +0700

    Update tungau.py

--- userbot/modules/tungau.py
+++ userbot/modules/tungau.py
@@ -18,3 +18,12 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 opener = urllib.request.build_opener()
 useragent = 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.70 Mobile Safari/537.36'
 opener.addheaders = [('User-agent', useragent)]
+
+
+
+@register(outgoing=True, pattern="^.ainna$")
+async def repo_is_here(wannasee):
+    """ For .ainna command, just returns the ainna URL. """
+    await wannase.edit(
+        " **INSTAGRAM: ** [KLICK FOR MY INSTAGRAM AINNA](https: // www.instagram.com/ainnaaa.yr)"
+    )
