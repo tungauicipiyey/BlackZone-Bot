45236f8777ca5fbe8827ade4a4e24078e082510d
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-05-01 00:36:57 +0700

    Update tungau.py

--- userbot/modules/tungau.py
+++ userbot/modules/tungau.py
@@ -1,4 +1,3 @@
-
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
@@ -7,8 +6,22 @@
 # You can find misc modules, which dont fit in anything xD
 """ Userbot module for other small commands. """
 
-from userbot import ALIVE_NAME
+from random import randint
+from time import sleep
+from os import execl
+import asyncio
+import sys
+import os
+import io
+import sys
+from userbot import ALIVE_NAME, BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
+from userbot.events import register
+from userbot.utils import time_formatter
 import urllib
+import requests
+from bs4 import BeautifulSoup
+import re
+from PIL import Image
 
 
 # ================= CONSTANT =================
@@ -18,6 +31,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 opener = urllib.request.build_opener()
 useragent = 'Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.70 Mobile Safari/537.36'
 opener.addheaders = [('User-agent', useragent)]
+
 
 
 @register(outgoing=True, pattern="^.ainna$")
