0c0778857f3bdc86d07631ab24b11821b5dc9c57
tungauicipiyey <80462153+tungauicipiyey@users.noreply.github.com>
2021-12-13 13:47:27 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -101,9 +101,9 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/tungauicipiyey/Virus-UserBot.git")
+    "https://github.com/tungauicipiyey/BlackZone-Bot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Virus-UserBot")
+    "UPSTREAM_REPO_BRANCH", "BlackZone-Bot")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
